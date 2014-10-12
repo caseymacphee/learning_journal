1b36a948b1c8e6dbe683282745f7d5a97bf2dda8
Casey MacPhee <caseymacphee@gmail.com>
2014-10-12 15:44:14 -0700

    created a view and database testing for entry and retrieval

--- journal.py
+++ journal.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+import datetime
 import os
 from flask import Flask
 import psycopg2
 from contextlib import closing
 from flask import g
-
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -39,10 +40,28 @@ def teardown_request(exception):
 		else:
 			db.commit()
 		db.close()
-	
+
+def write_entry(title, text):
+	if not title or not text:
+		raise ValueError("Title and text required for writing and entry")
+	con = get_database_connection()
+	cur = con.cursor()
+	now = datetime.datetime.utcnow()
+	cur.execute(DB_ENTRY_INSERT, [title, text, now])
+
+def get_all_entries():
+	"""return a list of all entries as dicts"""
+	con = get_database_connection()
+	cur = con.cursor()
+	cur.execute(DB_ENTRIES_LIST)
+	keys = ('id', 'title', 'text', 'created')
+	return [dict(zip(keys, row)) for row in cur.fetchall()]
+
+
 @app.route('/')
-def hello():
-	return u'Hello world!'
+def show_entries():
+	entries = get_all_entries()
+	return render_template('list_entries.html', entries=entries)
 	
 
 DB_SCHEMA = """
@@ -54,5 +73,12 @@ CREATE TABLE entries (
 	created TIMESTAMP NOT NULL
 )
 """
+DB_ENTRY_INSERT = """
+INSERT INTO entries (title, text, created) VALUES(%s, %s, %s)
+"""
+DB_ENTRIES_LIST = """
+SELECT id, title, text, created FROM entries ORDER BY created DESC
+"""
+
 if __name__ == '__main__':
 	app.run(debug=True)
